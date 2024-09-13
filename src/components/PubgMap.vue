b76f31dffb03be2eaa2e01f690cb199e3873b78e
Tiluk <91837678+tiluk@users.noreply.github.com>
2024-09-13 21:07:25 +0200

    Heat map overlay (#2)

--- src/components/PubgMap.vue
+++ src/components/PubgMap.vue
@@ -1,21 +1,74 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import * as L from 'leaflet'
+import { nextTick, onMounted, ref, watch } from 'vue'
+import 'leaflet.heat'
+import 'leaflet/dist/leaflet.css'
 
 const props = defineProps<{
-  map: string
+  selectedMap: string
 }>()
+const leafletMap = ref<L.Map>()
+const heat = ref<L.HeatLayer>()
+const heatLatLngs = ref<L.LatLng[]>([])
+const playerCount = ref<number>(100)
 
 const toMapPath = (map: string) => `src/assets/maps/${map.toLocaleLowerCase()}/Low_Res.png`
 
-onMounted(() => {})
+const initMap = () => {
+  const bounds: L.LatLngBoundsLiteral = [
+    [0, 0],
+    [1000, 1000]
+  ]
+  leafletMap.value = L.map('pubgmap', {
+    crs: L.CRS.Simple,
+    minZoom: 0,
+    maxZoom: 2,
+    attributionControl: false,
+    zoomControl: false,
+    maxBoundsViscosity: 0.9,
+    maxBounds: bounds,
+    doubleClickZoom: false
+  })
+  L.imageOverlay(toMapPath(props.selectedMap), bounds).addTo(leafletMap.value)
+  leafletMap.value.fitBounds(bounds)
+
+  heatLatLngs.value = []
+
+  heat.value = L.heatLayer([], {
+    radius: 25
+  }).addTo(leafletMap.value)
+
+  leafletMap.value.on('click', (e: L.LeafletMouseEvent) => {
+    e.latlng.alt = 25 / playerCount.value
+    heatLatLngs.value.push(e.latlng)
+    heat.value?.setLatLngs(heatLatLngs.value)
+  })
+}
+
+onMounted(() => {
+  nextTick(() => {
+    initMap()
+  })
+})
+
+watch(
+  () => props.selectedMap,
+  () => {
+    leafletMap.value?.remove()
+    heat.value?.remove()
+    initMap()
+  }
+)
 </script>
 
 <template>
-  <v-img :src="toMapPath(props.map)"></v-img>
+  <div id="pubgmap" class="mapContainer"></div>
 </template>
 
 <style scoped lang="scss">
-.v-img {
+.mapContainer {
   height: 100vh;
+  overflow: hidden;
+  background-color: black;
 }
 </style>
